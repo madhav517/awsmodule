331135638efeb595e67d003699775ad15097c447
madhav517 <madhav517@gmail.com>
2023-09-16 15:44:10 +0200

    Backend S3

--- test/main.tf
+++ test/main.tf
@@ -14,7 +14,11 @@ provider "aws" {
    access_key = var.access_key
    secret_key = var.secret_key
 }
-
+backend "s3" {
+   bucket = "jenkinsterraformbucket"
+   key    = "state"
+   region = "ap-south-1"
+ }
 module "ec2" {
   source      = "../modules/vm"
   vpc_security_groupids = "${module.securitygrp.security_groupid}"
